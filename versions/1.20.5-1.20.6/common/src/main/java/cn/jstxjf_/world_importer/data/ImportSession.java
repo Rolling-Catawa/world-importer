54332c787532eedcb49102c999ae9d036646b856
Rolling-Catawa <yaozj6419@gmail.com>
2026-02-23 16:23:08 +0800

    Fix release JAR names with MC version, fix NeoForge annotation, fix README aliases, rewrite UTF-8 sources

--- versions/1.20.5-1.20.6/common/src/main/java/cn/jstxjf_/world_importer/data/ImportSession.java
+++ versions/1.20.5-1.20.6/common/src/main/java/cn/jstxjf_/world_importer/data/ImportSession.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-
 public class ImportSession {
     private static final Map<MinecraftServer, ImportSession> SESSIONS = new ConcurrentHashMap<>();
 
--- versions/1.20.5-1.20.6/neoforge/src/main/java/cn/jstxjf_/world_importer/neoforge/WorldImporterNeoForgeClient.java
+++ versions/1.20.5-1.20.6/neoforge/src/main/java/cn/jstxjf_/world_importer/neoforge/WorldImporterNeoForgeClient.java
@@ -3,10 +3,11 @@ package cn.jstxjf_.world_importer.neoforge;
 import cn.jstxjf_.world_importer.client.WorldImporterClient;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 
-@Mod.EventBusSubscriber(modid = "world_importer", bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+@EventBusSubscriber(modid = "world_importer", bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class WorldImporterNeoForgeClient {
     @SubscribeEvent
     public static void onClientSetup(FMLClientSetupEvent event) {
